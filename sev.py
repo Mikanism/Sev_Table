1f6584c779038da74350c799a8188b2ef41ab426
Mikanism <96637828+Mikanism@users.noreply.github.com>
2023-06-06 05:20:34 +0300

    Update sev.py

--- sev.py
+++ sev.py
@@ -1,15 +1,7 @@
 import streamlit as st
 import sqlite3
 import pandas as pd
-
-filds = ['Создана', 'Номер договора', 'Адрес объекта оценки',
-       'ИНФОРМАЦИЯ ПО ОТЧЕТУ: Город', 'ИНФОРМАЦИЯ ПО ОТЧЕТУ: Округ',
-       'ИНФОРМАЦИЯ ПО ОТЧЕТУ: Район', 'ИНФОРМАЦИЯ ПО ОТЧЕТУ: Площадь',
-       'ИНФОРМАЦИЯ ПО ОТЧЕТУ: Год постройки',
-       'ИНФОРМАЦИЯ ПО ОТЧЕТУ: Материал стен',
-       'ИНФОРМАЦИЯ ПО ОТЧЕТУ: Этажность', 'ИНФОРМАЦИЯ ПО ОТЧЕТУ: Отделка',
-       'ИНФОРМАЦИЯ ПО ОТЧЕТУ: Комнатность',
-       'ИНФОРМАЦИЯ ПО ОТЧЕТУ: Стоимость 1 кв.м']
+import plotly.express as px
 
 filds1 = ['ИНФОРМАЦИЯ ПО ОТЧЕТУ: Площадь',
        'ИНФОРМАЦИЯ ПО ОТЧЕТУ: Год постройки',
@@ -17,7 +9,7 @@ filds1 = ['ИНФОРМАЦИЯ ПО ОТЧЕТУ: Площадь',
        'ИНФОРМАЦИЯ ПО ОТЧЕТУ: Этажность', 'ИНФОРМАЦИЯ ПО ОТЧЕТУ: Отделка',
        'ИНФОРМАЦИЯ ПО ОТЧЕТУ: Комнатность']
 
-cities = ['','Красногорск', 'Видное', 'Одинцово', 'Люберцы', 'Мытищи', 'Балашиха',
+cities = ['Красногорск', 'Видное', 'Одинцово', 'Люберцы', 'Мытищи', 'Балашиха',
         'Химки', 'Котельники', 'Домодедово', 'Солнечногорск', 'Пушкино',
         'Реутов', 'Щёлково', 'Подольск', 'Наро-Фоминск', 'Серпухов', 'Истра',
         'Долгопрудный', 'Дмитров', 'Раменское', 'Чехов', 'Королёв', 'Ногинск',
@@ -30,22 +22,15 @@ cities = ['','Красногорск', 'Видное', 'Одинцово', 'Лю
         'Красноармейск', 'Зарайск', 'Электрогорск', 'Черноголовка', 'Пущино',
         'Дедовск']
 
-
-
-
-
-
-
-
-st.title('Название?')
+st.title('Аналитика Sevial')
 
 main, load = st.tabs(['Таблица', 'Загрузить'])
-
 
 with load:
     @st.cache_data
     def load_data(file):
         df = pd.read_excel(file)
+        df['Создана'] = df['Создана'].dt.date
         df.to_sql('info', con)
         
     file = st.file_uploader('Загрузите xlsx Файл')
@@ -56,15 +41,16 @@ with load:
     if file is not None:
         load_data(file)
 
+    cc1, cc2 = st.columns([3, 1])
+    with cc2:
+        reloud = st.button('Перезагрузить')
+        if reloud:
+            st.cache_data.clear()
+            st.experimental_rerun()
+
 with main:
     if "disabled" not in st.session_state:
-        st.session_state["disabled"] = False
-
-    def disable():
-        if city == '':
-            st.session_state["disabled"] = True
-        else:
-            st.session_state["disabled"] = False
+        st.session_state["disabled"] = True
 
     def districts(area):
         sql_d = f'SELECT "ИНФОРМАЦИЯ ПО ОТЧЕТУ: Район" FROM info WHERE "ИНФОРМАЦИЯ ПО ОТЧЕТУ: Округ" = "{area}" GROUP BY "ИНФОРМАЦИЯ ПО ОТЧЕТУ: Район"'
@@ -75,12 +61,12 @@ with main:
     try:
         c1, c2, c3 = st.columns(3)
         with c1:
-            city = st.selectbox('Город', cities, on_change = disable)
+            city = st.selectbox('Город', cities, disabled = st.session_state.disabled)
+            st.checkbox('Москва', key= "disabled")
         with c2:
-            area = st.selectbox('Округ', ['НАО', 'ЗАО', 'ЮВАО', 'САО', 'СЗАО', 'ЮАО', 'СВАО', 'ЦАО', 'ВАО','ЮЗАО', 'ЗелАО', 'ТАО'], disabled = st.session_state.disabled)
+            area = st.selectbox('Округ', ['НАО', 'ЗАО', 'ЮВАО', 'САО', 'СЗАО', 'ЮАО', 'СВАО', 'ЦАО', 'ВАО','ЮЗАО', 'ЗелАО', 'ТАО'], disabled = not st.session_state.disabled)
         with c3:
-            district = st.selectbox('Район', districts(area), disabled = st.session_state.disabled)
-
+            district = st.selectbox('Район', districts(area), disabled = not st.session_state.disabled)
         st.divider()
 
         col1, col2, col3 = st.columns(3)
@@ -96,17 +82,13 @@ with main:
             remont = st.multiselect('Отделка', ['простая', 'без отделки', 'евро'])
             rooms = st.multiselect('Комнатность', [1.0, 2.0, 3.0, 4.0, 5.0])
 
+        upload = st.button("Показать")
         st.divider()
-        upload = st.button("Сохранить")
-
-
 
         if upload:
-            # sql = 'SELECT * FROM info WHERE
-            # pd.read_sql(sql, con, index_col=['index'])
             sql = 'SELECT "Создана", "Номер договора", "ИНФОРМАЦИЯ ПО ОТЧЕТУ: Стоимость 1 кв.м", "Адрес объекта оценки", '
             
-            if st.session_state.disabled:
+            if not st.session_state.disabled:
                 sql += '"ИНФОРМАЦИЯ ПО ОТЧЕТУ: Город"'
             else:
                 sql += '"ИНФОРМАЦИЯ ПО ОТЧЕТУ: Округ", "ИНФОРМАЦИЯ ПО ОТЧЕТУ: Район"'
@@ -114,17 +96,14 @@ with main:
             sql += ''.join(f', "{i}"' for i in filds1)
             sql += ' FROM info WHERE '
 
-            if st.session_state.disabled:
+            if not st.session_state.disabled:
                 sql += f'"ИНФОРМАЦИЯ ПО ОТЧЕТУ: Город" = "{city}" AND '
             elif district == 'ВСЕ':
                 sql += f'"ИНФОРМАЦИЯ ПО ОТЧЕТУ: Округ" = "{area}" AND '
             else:
                 sql += f'"ИНФОРМАЦИЯ ПО ОТЧЕТУ: Округ" = "{area}" AND "ИНФОРМАЦИЯ ПО ОТЧЕТУ: Район" = "{district}" AND '
 
-
-            
             ans = [square, year, material, floar, remont, rooms]
-            
 
             for i in range(len(filds1)):
                 if ans[i] != []:
@@ -135,9 +114,27 @@ with main:
                     sql += ') AND '
             sql = sql[:-5]
 
-            st.dataframe(pd.read_sql(sql, con)) #index_col = ['index']
+            df = pd.read_sql(sql, con)
+            if len(df) > 1:
+                st.dataframe(df)
+                chart = df[['Создана', 'ИНФОРМАЦИЯ ПО ОТЧЕТУ: Стоимость 1 кв.м']].sort_values('Создана')
+                chart['ИНФОРМАЦИЯ ПО ОТЧЕТУ: Стоимость 1 кв.м'] = chart['ИНФОРМАЦИЯ ПО ОТЧЕТУ: Стоимость 1 кв.м'].apply(lambda x: x.replace(' ', '').replace('р.', '').replace('р', '')).astype(float).astype(int)
+
+                minn = '{0:,}'.format(chart["ИНФОРМАЦИЯ ПО ОТЧЕТУ: Стоимость 1 кв.м"].min()).replace(',', ' ')
+                meann = '{0:,}'.format(chart["ИНФОРМАЦИЯ ПО ОТЧЕТУ: Стоимость 1 кв.м"].mean().astype(int)).replace(',', ' ')
+                maxx =  '{0:,}'.format(chart["ИНФОРМАЦИЯ ПО ОТЧЕТУ: Стоимость 1 кв.м"].max()).replace(',', ' ')
+
+                fig = px.line(chart, x = 'Создана', y = 'ИНФОРМАЦИЯ ПО ОТЧЕТУ: Стоимость 1 кв.м', color_discrete_sequence = ["#ff0000"], 
+                            title = f'Мин. цена = {minn}р.\
+                                Ср. цена = {meann}р.\
+                                Макс. цена = {maxx}р.')
+                st.plotly_chart(fig)
+
+            elif len(df) == 1: 
+                st.dataframe(df)
+            else:
+               a1, a2, a3 =  st.columns(3)
+               a2.header('Нет данных')
+
     except sqlite3.OperationalError:
             st.error('Загрузите базу данных')
-
-
-
